6f6e68b535eca96a1cc59ee0b6646c7b81a35acd
Vinicius P <vinicius.rochasherri7638@gmail.com>
2026-02-07 10:39:15 -0300

    Add files via upload

---
+++ aula7_exercicio.py
@@ -0,0 +1,22 @@
+#**Exercício 0:** Escreva um programa que use a função `range()` para gerar os números pares de 2 a 20 e, em seguida, imprima cada número.
+for num in range(2, 21, 2):
+    print(num)
+
+#**Exercício 1:** Crie uma lista chamada numeros que contenha os números inteiros de 1 a 10 e imprima-a na tela.
+x = num_lista = [1, 2, 3, 4, 5, 6, 7, 8, 9,10,]
+print(x)
+
+#**Exercício 2:** Acesse e imprima o terceiro elemento da lista `numeros`.
+print(x[3])
+
+#**Exercício 3:** Adicione o número 9 à lista `numeros` e imprima a lista atualizada.
+num_lista.append(9)
+print(x)
+#**Exercício 4:** Remova o número 5 da lista `numeros` e imprima a lista resultante.
+num_lista.remove(5)
+print(x)
+
+#**Exercício 5:** Crie uma lista chamada carros contendo três nomes de carros diferentes. Em seguida, concatene essa lista com a lista `numeros` e imprima o resultado.
+c = carr_lista = ['Volkswagen', 'Fiat', 'Chevrolet',]
+c.append(x)
+print(c)
